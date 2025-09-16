bdbcf7944d14b80678729b813d206b967e113743
K <K@Ahmads-MacBook-Air.local>
2025-09-17 00:02:15 +0300

    v2

--- QCS.py
+++ QCS.py
@@ -10,10 +10,9 @@ def checks(x):
     a = (len(x) == 11)
     b = ((x[2] == '_') and (x[5] == '_'))
     x.pop(2) and x.pop(4)
+    c = True
     for y in x:
-        if y.isdigit():
-            c = True
-        else: 
+        if not y.isdigit():
             c = False
             break
     if a + b + c == 3:
@@ -23,47 +22,58 @@ def checks(x):
 def tag(x):
     subprocess.run(['tag', '-a', 'Red', str(x)])
 
-# target & contents extract
+# Setup (target and toTag)
 target = Path('/Users/K/Desktop/Example')
 paths_contents = listdir(target)
-names_contents = sorted([x.name for x in paths_contents])
+toTag = []
 
-# check#1 Type -- (make sure only folders ∈ contents)
+# Type Check -- (make sure only folders ∈ contents)
 check1 = True
 for x in paths_contents:
     if x.is_file():
         check1 = False  
 if not check1:
-    tag(x)
+    toTag.append(x)
 
-# check#2 Type & Naming Format -- (subfolders & contents)
+# Subfolder Check
+for x in paths_contents:
+    moveOn = False
 
-for x in names_contents:
-
-    # (Subfolder Name Format)
-    subfolder_name = [y for y in x]
-    if checks(subfolder_name) == False:
-        tag(target / x)
-
-    # (Contents ∈ Subfolder)
-    subfolder_path = target / x
-    if subfolder_path.is_dir(): 
-        subfolder_contents = listdir(subfolder_path)
-    subfolder_contents.discard(subfolder_path / 'CaptureOne')
-    subfolder_contents.discard(subfolder_path / f'{x}_Marking.txt') # Change txt to CR3
+    # Setup (contents)
+    if x.is_dir(): 
+        subfolder_contents = listdir(x)
+    subfolder_contents.discard(x / 'CaptureOne')
+    subfolder_contents.discard(x / f'{x}_Marking.txt') # Change txt to CR3
     file_stems = [y.stem for y in subfolder_contents]
     file_suffixes = {y.suffix for y in subfolder_contents}
+
+    # Subfolder Name Format
+    subfolder_name = [y for y in x.name]
+    if checks(subfolder_name) == False:
+        toTag.append(x)
+        moveOn = True
+    if moveOn:
+        continue
+
+        # Content Quantity Check
+    if len(subfolder_contents) == 0:
+        toTag.append(x)
+        moveOn = True
+    if moveOn:
+        continue
 
         # Contents type check
     for y in subfolder_contents:
         if y.is_dir():
-            tag(subfolder_path)
+            toTag.append(x)
+            moveOn = True
+    if moveOn:
+        continue
         
         # File Stem length check
-    moveOn = False
     for y in file_stems:
         if not (len(y) == 14):
-            tag(subfolder_path)
+            toTag.append(x)
             moveOn = True
     if moveOn:
         continue
@@ -71,30 +81,39 @@ for x in names_contents:
         # Underscore preceeding numbering check
     for y in file_stems:
         if not (y[11] == '_'):
-            tag(subfolder_path)
+            toTag.append(x)
+            moveOn = True
+    if moveOn:
+        continue    
 
         # Suffix Type check
-    if not (file_suffixes == {'.CR3'}):
-        #tag(subfolder_path)
-        pass
-
+    if not (file_suffixes == {'.txt'}):
+         toTag.append(x)
+         moveOn = True
+    if moveOn:
+        continue
+       
         # Subfolder & Contents Name Correspondence 
     for y in file_stems:
-        if not (y[0:11] == x):
-            tag(subfolder_path)
-        
+        if not (y[0:11] == x.name):
+            toTag.append(x)
+            moveOn = True
+    if moveOn:
+        continue
+            
         # Order of Numbers
-    base = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', 
- '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
- '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
- '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
- '41', '42', '43', '44', '45', '46', '47', '48', '49', '50']
     numbering = sorted([y[12:14] for y in file_stems])
-    del base[len(numbering):50]      
+    base = []
+    for y in range(1,len(numbering)+1):
+        base.append(f"{y:02d}")     
     if not (numbering == base):
-        tag(subfolder_path)
-
+        toTag.append(x)
+        moveOn = True
+    if moveOn:  
+        continue
+ 
+# Tagging & Result
+for y in toTag:
+    tag(y)
 print(f'\nCheck Complete!\n')
-    
 
-  
