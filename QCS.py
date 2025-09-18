832d60f07ae4c149f39ecb680791c0c183b04d34
K <K@Ahmads-MacBook-Air.local>
2025-09-19 01:25:21 +0300

    simplified subfolder conditionals

--- QCS.py
+++ QCS.py
@@ -21,21 +21,23 @@ def checks(x):
         return False
 def tag(x):
     subprocess.run(['tag', '-a', 'Red', str(x)])
+def error(x,y):
+    toTag.append(x)
+    issues.append(f"{x.name} triggered {y} Error")
 
-# Setup (target and toTag)
+# Setup
 target = Path(input("\nTarget : "))
 paths_contents = listdir(target)
 toTag = []
+issues = []
 
 # Type Check -- (make sure only folders ∈ contents)
 check1 = True
 for x in paths_contents:
     if x.is_file():
-        check1 = False  
-if not check1:
-    toTag.append(x)
+        error(x, 'Content Type')
 
-# Subfolder Check
+# Subfolder Checks
 for x in paths_contents:
     moveOn = False
 
@@ -43,77 +45,68 @@ for x in paths_contents:
     if x.is_dir(): 
         subfolder_contents = listdir(x)
     subfolder_contents.discard(x / 'CaptureOne')
-    subfolder_contents.discard(x / f'{x.name}_Marking.CR3') # Change txt to CR3
+    subfolder_contents.discard(x / f'{x.name}_Marking.txt') # txt ↔ CR3
     file_stems = [y.stem for y in subfolder_contents]
     file_suffixes = {y.suffix for y in subfolder_contents}
 
     # Subfolder Name Format
     subfolder_name = [y for y in x.name]
     if checks(subfolder_name) == False:
-        toTag.append(x)
-        moveOn = True
-    if moveOn:
+        error(x,'Name Format')
         continue
 
-        # Content Quantity Check
+        # Content Existence Check
     if len(subfolder_contents) == 0:
-        toTag.append(x)
-        moveOn = True
-    if moveOn:
+        error(x,'Content Existence')
         continue
 
         # Contents type check
     for y in subfolder_contents:
         if y.is_dir():
-            toTag.append(x)
-            moveOn = True
-    if moveOn:
-        continue
+            error(x,'Sub-Content Type')
+            continue
         
         # File Stem length check
     for y in file_stems:
         if not (len(y) == 14):
-            toTag.append(x)
-            moveOn = True
-    if moveOn:
-        continue
+            error(x,'Name Length')
+            continue
 
         # Underscore preceeding numbering check
     for y in file_stems:
         if not (y[11] == '_'):
-            toTag.append(x)
-            moveOn = True
-    if moveOn:
-        continue    
+            error(x,'Formatting')
+            continue    
 
         # Suffix Type check
-    if not (file_suffixes == {'.CR3'}):
-         toTag.append(x)
-         moveOn = True
-    if moveOn:
-        continue
+    if not (file_suffixes == {'.txt'}):  # txt ↔ CR3
+         error(x,'Suffix')
+         continue
        
         # Subfolder & Contents Name Correspondence 
     for y in file_stems:
         if not (y[0:11] == x.name):
-            toTag.append(x)
-            moveOn = True
-    if moveOn:
-        continue
-            
+            error(x,'Correspondence')
+            continue     
+                          
         # Order of Numbers
     numbering = sorted([y[12:14] for y in file_stems])
     base = []
     for y in range(1,len(numbering)+1):
         base.append(f"{y:02d}")     
     if not (numbering == base):
-        toTag.append(x)
-        moveOn = True
-    if moveOn:  
+        error(x,'Numbering')
         continue
  
 # Tagging & Result
 for y in toTag:
     tag(y)
+issues.sort()
+
 print(f'\nCheck Complete!\n')
 
+details = input("Detailed Report (y/n) : ").strip().lower()
+if details == 'y':
+    for x in issues:
+        print('\n', x)
+print()
